68058813a6b78c267371c9ddd89cafc6fa222084
Antonio Piatti <antonio.piattifadda@gmail.com>
2025-08-15 15:53:14 -0300

    fix: vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // Configuración adicional para Vercel
+  base: '/',
+  build: {
+    outDir: 'dist',
+  }
 })
