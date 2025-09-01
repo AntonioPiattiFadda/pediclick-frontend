531cc6070f7ab0deddde5f3716a1c9703533e963
Antonio Piatti <antonio.piattifadda@gmail.com>
2025-09-01 08:02:28 -0300

    Add: types

--- src/types/loadOrder.tsx
+++ src/types/loadOrder.tsx
@@ -1,24 +1,35 @@
 export type LoadOrder = {
-  load_order_id: string;
-  load_order_number: string;
+  load_order_id?: string;
+  load_order_number: string; //Input tipo numero
   //Lo tiene el remito del proveedor
-  provider_id: number;
-  //   domicilio: string;
-  //   cuit: string;
+  provider_id: number | null; //Al seleccionar se despliegan adderess y ciuit abjo que vienen por base dde datos
+  //   address: string | null;
+  //   cuit: string | null;
+  //Te va a dar error en base de datos porque no esta creado el campo address ni cuit
   // puede tener un codigo porque pueden ser muchos proveedores.
+  // Al lado tiene el boton de + nuevo de proveedor
+
   delivery_date: string; // Auytomaticamente seleccionar la fecha de hoy sino que el cliente la pueda cambiar
+  //Fijate como ejemplo el cenvimiento del prodcutso en lote
   receptor_id: number | null;
-  //   tiene que elegir entre los empleados o poner otro y agregar un string
+  // Tiene que elegir entre los empleados o poner otro y agregar un string.
+  // No esta hecho.
+  // Tiene que haber la opcion de otro y esee campo es un string Como primera opcion pones otro.
+
   transporter_data: {
-    delivery_company: string; // CHANGE TO ENGLISH
-    name: string;
-    licence_plate: string; // CHANGE TO ENGLISH
+    delivery_company: string | null; 
+    name: string | null;
+    licence_plate: string | null; 
     // otros campos que pueda tener el transportista
+    // Todo en la misma linea
   };
+
+
 
   delivery_price: number | null;
 
   assigned_to: number | null; // El lugar deposito o receptor o limbo
+  // Esto todavia no lo hagas
 
   invoice_number: number | null;
 
